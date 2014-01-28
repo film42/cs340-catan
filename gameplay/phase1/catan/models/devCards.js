615e0426974bb7de826aa1afe28d2f0f59b0b2fd
todoakaio <todoakaio@gmail.com>
2014-01-27 19:23:01 -0700

    Added DevCards.js
    Added DevCards.js, it’s pretty much the same as Deck.js

--- gameplay/phase1/catan/models/devCards.js
+++ gameplay/phase1/catan/models/devCards.js
@@ -3,12 +3,75 @@ catan.models = catan.models || {};
 
 catan.models.DevCards = (function() {
 
-  // Constructor 
+  /**
+  @author Steve Allred
+  DevCards contains values of various development cards.
+  Domain: 
+    JSON: This should contain the initial values of our resources.
+    
+  Read-only:
+    The player object on the client side is read-only.  The server will update the client side model with a new version.
+    
+  Constructor Specification:
+    PRE: the JSON provides values for each of the resources.  All other data is either calculated or generated.
+    
+    POST: The constructor creates an object that has all of the following fields initialized:
+      monopoly : number
+      monument : number
+      roadBuilding : number
+      soldier : number
+      yearOfPlenty : number
+    
+    @class DevCards
+    @constructor
+    @param {JSON} the data containing the initialized objects
+*/
   function DevCards(json) {
 
   }
+  /**
+  Gets the Monopoly card count.
 
-  DevCards.prototype.example = function() {
+    @class DevCards
+    @param {JSON} the data containing the initialized objects
+  */
+  DevCards.prototype.getMonopolyCardCount = function() {
+      return null;
+  };
+  /**
+  Gets the Monument card count.
+
+    @class DevCards
+    @param {JSON} the data containing the initialized objects
+  */
+  DevCards.prototype.getMonumentCardCount = function() {
+      return null;
+  };
+  /**
+  Gets the roadBuilding card count.
+
+    @class DevCards
+    @param {JSON} the data containing the initialized objects
+  */
+  DevCards.prototype.getRoadBuildingCardCount = function() {
+      return null;
+  };
+  /**
+  Gets the Soldier card count.
+
+    @class DevCards
+    @param {JSON} the data containing the initialized objects
+  */
+  DevCards.prototype.getSoldierCardCount = function() {
+      return null;
+  };
+  /**
+  Gets the year of plenty card count.
+
+    @class DevCards
+    @param {JSON} the data containing the initialized objects
+  */
+  DevCards.prototype.getYearOfPlentyCardCount = function() {
       return null;
   };
 
