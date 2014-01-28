b61b33355c9a2391707c43bc2d9093ffbc15814f
Garrett Thornburg <film42@gmail.com>
2014-01-27 19:31:42 -0700

    dice docs

--- gameplay/phase1/catan/util/dice.js
+++ gameplay/phase1/catan/util/dice.js
@@ -2,7 +2,23 @@ var catan = catan || {};
 catan.util = catan.util || {};
 catan.util.dice = catan.util.dice || {};
 
-// We don't need to instantiate, we're just calling roll
-catan.util.dice.roll = function rollDie() {
+/**
+  This module contains the game's util functinos
+
+  @module                catan.models
+  @namespace             catan.models
+*/
+
+/**
+  Play soldier card
+  <pre>
+  PRE: None
+  POST: Receive a number between 1 - 6
+  </pre>
+   
+  @method rollDie
+  @return {integer} the result of the die roll
+*/
+catan.util.dice.rollDie = function() {
   return 2;
 };
