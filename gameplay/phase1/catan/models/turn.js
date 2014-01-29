588550c6964905ff3bf9391c34d4d335b3026b6c
junetang <junetanglu@yahoo.com>
2014-01-28 21:37:57 -0700

    fixed commits space problem

--- gameplay/phase1/catan/models/turn.js
+++ gameplay/phase1/catan/models/turn.js
@@ -3,123 +3,124 @@ catan.models = catan.models || {};
 
 catan.models.Turn = (function() {
 /**
-        The model class keeps current turn state and its player's ID
-        <pre>      
-        Domain:
-            currentTurn: Player's id, Number
-            status:  Status, Stirng
+  The model class keeps current turn state and its player's ID
+  <pre>      
+  Domain:
+  currentTurn: Player's id, Number
+  status:  Status, Stirng
          
-        Constructor Specification:
-            PRE: !isNaN(playerID)
-            PRE: !isNaN(playStatus)
-            POST: getPlayer() == playerID
-            POST: getStatus() == status
-        </pre>
+  Constructor Specification:
+  PRE: !isNaN(playerID)
+  PRE: !isNaN(playStatus) 
+  POST: getPlayer() == playerID
+  POST: getStatus() == status
+  </pre>
 
-        @class catan.models.Turn
-        @constructor
+  @class catan.models.Turn
+  @constructor
         
-        @param {Number} playerID 
-        @param {String} Status
-    */
+  @param {Number} playerID 
+  @param {String} Status
+*/
   function Turn(playerID, status) {
-		this.setCurrentTurn(playerID);
-		this.setStatus(status);
+    this.setCurrentTurn(playerID);
+    this.setStatus(status);
 
   }
   
   /**
-		Sets current turn to the player
-	    <pre>
-        PRE: !isNaN(playID)
-        </pre>
+    Sets current turn to the player
+    <pre>
+    PRE: !isNaN(playID)
+    </pre>
          
-        @method setCurrentTurn
-        @param {Number} playerID The player's ID
-	*/
+    @method setCurrentTurn
+    @param {Number} playerID The player's ID
+  */
 	
-	Turn.prototype.setCurrentTurn = function(playerID) {
-		this.currentTurn = playerID;
-	};
+  Turn.prototype.setCurrentTurn = function(playerID) {
+    this.currentTurn = playerID;
+  };
 	
-	 /**
-        Returns current player's ID
-		<pre>
-        PRE: None
-        </pre>
+  /**
+    Returns current player's ID
+	<pre>
+    PRE: None
+    </pre>
          
-        @method getCurrentTurn
-        @return {Number} player's ID    
-    */
-	Turn.prototype.getCurrentTurn = function() {
+    @method getCurrentTurn
+    @return {Number} player's ID    
+  */
+  Turn.prototype.getCurrentTurn = function() {
       return this.currentTurn;
-	};
+  };
 
-	/**
-		Sets the playing state
-		<pre>
-        PRE: !isNaN(status)
-        </pre>
+  /**
+  	Sets the playing state
+	<pre>
+    PRE: !isNaN(status)
+    </pre>
          
-        @method setStatus
-        @param {String} Status The playing status
-    */
+    @method setStatus
+    @param {String} Status The playing status
+  */
 	
-	Turn.prototype.setStatus = function(status) {
-		this.status = status;
-	};
+  Turn.prototype.setStatus = function(status) {
+    this.status = status;
+  };
 	
-	 /**
-        Returns the playing status
-        <pre>
-        PRE: None
-        </pre>
+  /**
+    Returns the playing status
+    <pre>
+    PRE: None
+    </pre>
          
-        @method getStatus
-        @return {String } status
-    */
-    Turn.prototype.getStatus = function() {
-      return this.status;
-	};
+    @method getStatus
+    @return {String } status
+  */
+  Turn.prototype.getStatus = function() {
+    return this.status;
+  };
 	
-	 /**
-        Returns ture if the status is playing state otherwise, false.
+  /**
+    Returns ture if the status is playing state otherwise, false.
 
-        @method isPlayingState
-        @return {boolean} true or false
-    */
-    Turn.prototype.isPlayingState() = function() {
+    @method isPlayingState
+    @return {boolean} true or false
+  */
+  Turn.prototype.isPlayingState() = function() {
       
-	};
+  };
 
-	/**
-        Returns ture if the status is rolling state otherwise, false.
+  /**
+    Returns ture if the status is rolling state otherwise, false.
          
-        @method isRollingState
-        @return {boolean} true or false
-    */
-    Turn.prototype.isRollingState() = function() {
+    @method isRollingState
+    @return {boolean} true or false
+  */
+  Turn.prototype.isRollingState() = function() {
       
-    };
+  };
 	
-	/**
-        Returns ture if the status is discard state otherwise, false.
+  /**
+    Returns ture if the status is discard state otherwise, false.
          
-        @method isDiscardState
-        @return {boolean} true or false
-    */
-    Turn.prototype.isDiscardState() = function() {
+    @method isDiscardState
+    @return {boolean} true or false
+  */
+  Turn.prototype.isDiscardState() = function() {
       
-    };
-    /**
-        Returns ture if the status is robbing state otherwise, false.
+  };
+  
+  /**
+    Returns ture if the status is robbing state otherwise, false.
          
-        @method isRobbing State
-        @return {boolean} true or false
-    */
-    Turn.prototype.isRobbingState() = function() {
+    @method isRobbing State
+    @return {boolean} true or false
+  */
+  Turn.prototype.isRobbingState() = function() {
       
-    };
+  };
 
   return Turn;
 })();
