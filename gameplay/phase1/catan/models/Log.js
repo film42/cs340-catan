38acdc881d2129ecdeadc42308e9d070501088c8
junetang <junetanglu@yahoo.com>
2014-01-28 21:46:08 -0700

    create log.js

---
+++ gameplay/phase1/catan/models/Log.js
@@ -0,0 +1,57 @@
+var catan = catan || {};
+catan.models = catan.models || {};
+catan.models.chat = catan.models.chat || {};
+
+catan.models.chat.Log= (function() {
+
+  /**
+    The model class for Log
+   
+    @class catan.models.chat.Log
+    @constructor
+    
+    @param {array} messages A list of objects with source(the player name attached to the event), message(the contents of the line)
+  */
+  function Log(messages) {
+    this.messages = messages;
+  }
+  
+  /**
+    Returns the array of messages 
+    <pre>
+    PRE: None
+    </pre>
+     
+    @method getmessages
+    @return {array} The array of message objects   
+  */
+  Chat.prototype.getMessages = function() {
+    return this.messages;
+  };
+  
+  /**
+    Add a message object to array of messages
+    <pre>
+      PRE: !isNaN()
+    </pre>
+
+    @method addMessage
+    @param {object} message object which contain message and source 
+  */
+  Chat.prototype.addMessage = function (message) {};
+    
+  /**
+    Returns the message object for given source 
+    <pre>
+      PRE: !isNaN(source)
+    </pre>
+       
+    @method getMessage
+    @return {object } message object which contain message and source for given source 
+  */
+  Chat.prototype.getMessage = function (source) {
+    return null;
+  };
+
+  return Chat;
+})();
