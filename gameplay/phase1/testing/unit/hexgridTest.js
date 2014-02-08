81484dae14d9ddf2c3770a9bf84dc5c95123f5ff
Adam <ajgoda90@gmail.com>
2014-02-07 17:27:50 -0700

    modified hexgridTest.js

--- gameplay/phase1/testing/unit/hexgridTest.js
+++ gameplay/phase1/testing/unit/hexgridTest.js
@@ -4,22 +4,29 @@ catan.models = catan.models || {};
 catan.models.map = catan.models.map || {};
 
 test("vertex.getValue", function(){
-  var vertex = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[][].vertexes[5]);
+  var vertex = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].vertexes[5]);
   var value = vertex.getValue();
-  ok(value.getBuildSite() == 1 && value.getOwnerID() == 2, "Occupied by Blue(ownerID = 2) Settlement");
+  ok(value.getBuildSite() == 1 && value.getOwnerID() == 2, "Occupied by Blue(ownerID = 2) Settlement(worth = 1)");
 });
 
 test("vertex.isOccupied", function(){
-  var vertex = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[][].vertexes[5]);
-  ok(vertex.isOccupied(), "Occupied by a player 2");
+  var vertex = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].vertexes[5]);
+  ok(vertex.isOccupied(), "True: Occupied by a player 2");
+  var vertex2 = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].vertexes[0]);
+  ok(!vertex2.isOccupied(), "False: unoccupied");
 });
 
 test("edge.getValue", function(){
-
+  var edge = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].edges[4]);
+  var value = edge.getValue();
+  ok(value.getOwnerID() == 2, "Occupied by Blue(ownerID = 2) road");
 });
 
 test("edge.isOccupied", function(){
-
+  var edge = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].edges[4]);
+  ok(edge.isOccupied(), "True: Occupied by a player 2");
+  var edge2 = new catan.models.map.Vertex(modelJson.map.hexGrid.hexes[3][3].edges[0]);
+  ok(!edge2.isOccupied(), "False: unoccupied");
 });
 
 test("hex.getLocation", function(){
