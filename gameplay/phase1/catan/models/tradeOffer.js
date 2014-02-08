a2c93a0351c8bcc04291b48ba7d6931557f30fbe
Jon George <jgeorge@byu.edu>
2014-02-07 21:23:11 -0700

    Added useful functions to TradeOffer

--- gameplay/phase1/catan/models/tradeOffer.js
+++ gameplay/phase1/catan/models/tradeOffer.js
@@ -41,7 +41,7 @@ catan.models.TradeOffer = (function() {
     @class Resources
     @return An integer that represents the Sender id
   */
-TradeOffer.prototype.getSender = function() {
+  TradeOffer.prototype.getSender = function() {
     return this.senderID;
   };
   /**
@@ -50,18 +50,73 @@ TradeOffer.prototype.getSender = function() {
     @class Resources
     @return An integer that represents the Receiver id
   */
-TradeOffer.prototype.getReceiver = function() {
+  TradeOffer.prototype.getReceiver = function() {
     return this.receiverID;
   };
   /**
-Gets the offer (a resource list) from this object.
+  Gets the offer (a resource list) from this object.
 
     @class Resources
     @return A resource list
   */
-TradeOffer.prototype.getOffer = function() {
+  TradeOffer.prototype.getOffer = function() {
     return this.resources;
   };
 
+  TradeOffer.prototype.getCardsOffered = function() {
+    var brick = 0;
+    var sheep = 0;
+    var wheat = 0;
+    var ore   = 0;
+    var wood  = 0;
+    
+    if(this.resources.getBrick() > 0)
+      brick = this.resources.getBrick();
+    
+    if(this.resources.getWood() > 0)
+      wood = this.resources.getWood();
+    
+    if(this.resources.getSheep() > 0)
+      sheep = this.resources.getSheep();
+    
+    if(this.resources.getOre() > 0)
+      ore = this.resources.getOre();
+    
+    if(this.resources.getWheat() > 0)
+      wheat = this.resources.getWheat();
+  
+    var ret = new catan.models.ResourceList({});
+    ret.setResourceListItems(brick, ore, sheep, wheat, wood);
+    return ret;
+  };
+  
+  TradeOffer.prototype.getCardsAskedFor = function() {
+    var brick = 0;
+    var sheep = 0;
+    var wheat = 0;
+    var ore = 0;
+    var wood = 0;
+    
+    if(this.resources.getBrick() < 0)
+      brick = -this.resources.getBrick();
+    
+    if(this.resources.getWood() < 0)
+      wood = -this.resources.getWood();
+    
+    if(this.resources.getSheep() < 0)
+      sheep = -this.resources.getSheep();
+    
+    if(this.resources.getOre() < 0)
+      ore = -this.resources.getOre();
+    
+    if(this.resources.getWheat() < 0)
+      wheat = -this.resources.getWheat();
+  
+    var ret = new catan.models.ResourceList({});s
+    ret.setResourceListItems(brick, ore, sheep, wheat, wood);
+    return ret;
+  };
+  
+  
   return TradeOffer;
 })();
