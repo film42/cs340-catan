a15778b8ac622cf845e951e40625cf0093a7ec00
Garrett Thornburg <film42@gmail.com>
2014-02-16 16:50:17 -0700

    truncontroller constructor

--- gameplay/js/turn_tracker/TrackerController.js
+++ gameplay/js/turn_tracker/TrackerController.js
@@ -8,41 +8,62 @@ The namespace for the turn tracker
 var catan = catan || {};
 catan.turntracker = catan.turntracker || {};
 
-catan.turntracker.Controller = (function turntracker_namespace() {	
+catan.turntracker.Controller = (function turntracker_namespace() {
 
-	var Controller = catan.core.BaseController;
+  var Controller = catan.core.BaseController;
     
-	/**
-		The controller class for the Turn Tracker
-		@class TurnTrackerController 
-		@extends misc.BaseController
-		@param {turntracker.View} view The view for this object to control.
-		@param {models.ClientModel} clientModel The clientModel for this object to control.
-		@constructor
-	**/
-	var TurnTrackerController = (function TurnTrackerController_Class(){
-	
-		function TurnTrackerController(view, clientModel){
-			Controller.call(this,view,clientModel);
+  /**
+    The controller class for the Turn Tracker
+    @class TurnTrackerController 
+    @extends misc.BaseController
+    @param {turntracker.View} view The view for this object to control.
+    @param {models.ClientModel} clientModel The clientModel for this object to control.
+    @constructor
+  **/
+  var TurnTrackerController = (function TurnTrackerController_Class(){
+  
+    function TurnTrackerController(view, clientModel){
+      Controller.call(this,view,clientModel);
             
-            // TODO: This constructor should configure its view by calling view.setClientColor and view.initializePlayer
-            // NOTE: The view.updateViewState and view.updatePlayer will not work if called from here.  Instead, these
-            //          methods should be called later each time the client model is updated from the server.
-		}
+      // TODO: This constructor should configure its view by calling view.setClientColor and view.initializePlayer
+      // NOTE: The view.updateViewState and view.updatePlayer will not work if called from here.  Instead, these
+      //          methods should be called later each time the client model is updated from the server.
 
-		core.forceClassInherit(TurnTrackerController,Controller);
+      this.view = view;
 
-		/**
-		 * Called by the view when the local player ends their turn.
-		 * @method endTurn
-		 * @return void
-		 */
-		TurnTrackerController.prototype.endTurn = function(){
-		}
-		
-		return TurnTrackerController;
-	} ());
+      var players = game.getPlayers();
 
-	return TurnTrackerController;
+      players.forEach(function(player) {
+
+        var color = player.getColor();
+        var id = player.getplayerID();
+        var name = player.getName();
+
+        view.initializePlayer(id, name, color);
+
+      });
+
+      game.addObserver(this.onUpdatedModel);
+    }
+
+    core.forceClassInherit(TurnTrackerController,Controller);
+
+    TurnTrackerController.prototype.onUpdatedModel = function() {
+
+    };
+
+    /**
+     * Called by the view when the local player ends their turn.
+     * @method endTurn
+     * @return void
+     */
+    TurnTrackerController.prototype.endTurn = function(){
+      game.finishTurn(function() {});
+    };
+    
+    return TurnTrackerController;
+  } ());
+
+  return TurnTrackerController;
 } ());
 
