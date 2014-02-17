7630f0fb1582688a3dbd6e038fc7c31e31aebab2
Garrett Thornburg <film42@gmail.com>
2014-02-17 12:40:05 -0700

    points controller points bug

--- gameplay/js/points/PointsController.js
+++ gameplay/js/points/PointsController.js
@@ -10,37 +10,38 @@ catan.points.Controller = catan.points.Controller || {};
 
 catan.points.Controller = (function VPController_Class(){
 
-	var Controller = catan.core.BaseController;
+  var Controller = catan.core.BaseController;
 
-	PointController.prototype = core.inherit(Controller.prototype);
+  PointController.prototype = core.inherit(Controller.prototype);
 
-	core.defineProperty(PointController.prototype, "GameFinishedView");
+  core.defineProperty(PointController.prototype, "GameFinishedView");
 
-	/** 
-		@class PointController
-		@constructor 
-		@extends misc.BaseController
-		@param {points.View} view
-		@param {misc.GameFinishedView} gameFinishedView
-		@param {models.Game} game
-	*/
-	function PointController(view, gameFinishedView, game){
-		this.setGameFinishedView(gameFinishedView);
-		Controller.call(this,view,game);
-		
-		gameFinishedView.setController(this);
-		view.setPoints(game.getPoints());
-		if (view.getPoints() > view.MAX_POINTS){
-		  //display the end-game view
-		  
-		}
-		
-		console.log("PointController Constructor Called");
-	}
+  /** 
+    @class PointController
+    @constructor 
+    @extends misc.BaseController
+    @param {points.View} view
+    @param {misc.GameFinishedView} gameFinishedView
+    @param {models.Game} game
+  */
+  function PointController(view, gameFinishedView, game){
+    this.setGameFinishedView(gameFinishedView);
+    Controller.call(this,view,game);
+    
+    gameFinishedView.setController(this);
+    var player = game.getCurrentPlayer();
+    view.setPoints(player.getPoints());
+    if (view.getTotalPoints() > view.MAX_POINTS){
+      //display the end-game view
+      
+    }
+    
+    console.log("PointController Constructor Called");
+  }
 
-	
-	
-	return PointController;	
+  
+  
+  return PointController;
 }());
 // STUDENT-REMOVE-END
 
