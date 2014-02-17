633d9671f101aab0e4ca1d8f7a3cc315d477f7e6
Garrett Thornburg <film42@gmail.com>
2014-02-17 14:46:09 -0700

    fixing up chat

--- gameplay/js/comms_box/CommController.js
+++ gameplay/js/comms_box/CommController.js
@@ -8,9 +8,9 @@ var catan = catan || {};
 catan.controllers = catan.controllers || {};
 
 catan.comm.Controller = (function () {
-	
-	var Controller = catan.core.BaseController;
-	
+  
+  var Controller = catan.core.BaseController;
+  
     /**
         The basic controller class to extend from
         @class BaseCommController 
@@ -19,88 +19,93 @@ catan.comm.Controller = (function () {
         @param {models.ClientModel} model The view for this object to control.
         @constructor
     **/
-	var BaseCommController = (function BaseCommController_Class(){
-		
-		BaseCommController.prototype = core.inherit(Controller.prototype);
-		BaseCommController.prototype.contructor = BaseCommController;
-		
-		function BaseCommController(logView, model){
-			Controller.call(this,logView,model);
-      
-		}
-		
-		return BaseCommController;
-	}());
-	
+  var BaseCommController = (function BaseCommController_Class(){
     
-	var LogController = (function LogController_Class(){
+    BaseCommController.prototype = core.inherit(Controller.prototype);
+    BaseCommController.prototype.contructor = BaseCommController;
+    
+    function BaseCommController(logView, model){
+      Controller.call(this,logView,model);
+      
+    }
+    
+    return BaseCommController;
+  }());
+  
+    
+  var LogController = (function LogController_Class(){
 
     LogController.prototype = core.inherit(BaseCommController.prototype);
-		LogController.prototype.constructor = LogController;
+    LogController.prototype.constructor = LogController;
 
-		/**
-		The controller class for the Log
-		@class LogController 
-		@constructor
-		@extends comm.BaseCommController
-		@param {comm.LogView} logView The view for this object to control.
-		@param {models.ClientModel} model The view for this object to control.
-		**/
-		function LogController(logView,model){
-			BaseCommController.call(this,logView,model);
-		}
+    /**
+    The controller class for the Log
+    @class LogController 
+    @constructor
+    @extends comm.BaseCommController
+    @param {comm.LogView} logView The view for this object to control.
+    @param {models.ClientModel} model The view for this object to control.
+    **/
+    function LogController(logView,model){
+      BaseCommController.call(this,logView,model);
+    }
 
-		LogController.prototype.onModelUpdate = function(){
+    LogController.prototype.onModelUpdate = function(){
      var logLines = this.getGame().getClientModel().getLog().getMessages();
      this.view.resetLines(logLines);
-    }
+    };
         
-		return LogController;
-	}());
-	
+    return LogController;
+  }());
+  
     
-	var ChatController = (function ChatController_Class(){
+  var ChatController = (function ChatController_Class(){
 
     ChatController.prototype = core.inherit(BaseCommController.prototype);
-		ChatController.prototype.constructor = ChatController;
+    ChatController.prototype.constructor = ChatController;
 
-		/**
-		The controller class for the Chat
-		@class ChatController 
-		@constructor
-		@extends comm.BaseCommController
-		@param {comm.ChatView} logView The view for this object to control.
-		@param {comm.ClientModel} model The view for this object to control.
-		**/
-		function ChatController(chatView,model){
-			BaseCommController.call(this,chatView,model);
-		}
+    /**
+    The controller class for the Chat
+    @class ChatController 
+    @constructor
+    @extends comm.BaseCommController
+    @param {comm.ChatView} logView The view for this object to control.
+    @param {comm.ClientModel} model The view for this object to control.
+    **/
+    function ChatController(chatView,game){
+      BaseCommController.call(this,chatView,game);
+
+      this.game = game;
+      this.view = chatView;
+
+      this.game.addObserver(this, this.onModelUpdate);
+    }
         
     /**
     This is the callback function passed into the game in order to update
     the views with the new model data.
     */
     ChatController.prototype.onModelUpdate = function(){
-     var chatLines = this.getGame().getClientModel().getChat().getMessages();
+     var chatLines = this.game.getModel().getChat().getMessages();
      this.view.resetLines(chatLines);
-    }
+    };
 
-		/**
-		Called by the view whenever input is submitted
-		@method addLine
-		@param {String} lineContents The contents of the submitted string
-		**/
-		ChatController.prototype.addLine = function(lineContents){
-		  this.getGame().getProxy().sendChat(lineContents, callback);
-		};
-		
-		return ChatController;
-	}());
-	
-	return {
-		LogController:LogController,
-		ChatController:ChatController
-	};
-	
+    /**
+    Called by the view whenever input is submitted
+    @method addLine
+    @param {String} lineContents The contents of the submitted string
+    **/
+    ChatController.prototype.addLine = function(lineContents){
+      this.game.sendChat(lineContents, function() {});
+    };
+    
+    return ChatController;
+  }());
+  
+  return {
+    LogController:LogController,
+    ChatController:ChatController
+  };
+  
 } ());
 
