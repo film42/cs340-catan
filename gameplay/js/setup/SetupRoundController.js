557eb1611e798cdaa9ba835c74579cbe58516117
junetang <junetanglu@yahoo.com>
2014-02-17 15:05:38 -0700

    implemented setupRoundController

--- gameplay/js/setup/SetupRoundController.js
+++ gameplay/js/setup/SetupRoundController.js
@@ -8,29 +8,49 @@ var catan = catan || {};
 catan.setup= catan.setup || {};
 
 catan.setup.Controller = (function(){
-	
-	var Controller = catan.core.BaseController;
+  
+  var Controller = catan.core.BaseController;
     
-	/** 
-		@class SetupRoundController
-		@constructor 
-		@extends misc.BaseController
-		@param {models.ClientModel} clientModel
-		@param {map.MapController} mapController
-	*/
-	var SetupRoundController = (function (){
-		
-		var SetupRoundController = function (clientModel, mapController){
-			this.mapController = mapController;
-			
-			Controller.call(this,undefined,clientModel);
-		};
-        
-		core.forceClassInherit(SetupRoundController,Controller);
-        
-		return SetupRoundController;
-	}());
+  /** 
+    @class SetupRoundController
+    @constructor 
+    @extends misc.BaseController
+    @param {models.ClientModel} clientModel
+    @param {map.MapController} mapController
+  */
+  var SetupRoundController = (function (){
     
-	return SetupRoundController;
+    var SetupRoundController = function (game, mapController){
+      this.mapController = mapController;
+      this.clientModel = clientModel;
+      
+       
+      Controller.call(this,undefined,game);
+      
+      var currentPlayerID = game.getCurrentPlayer();
+      var client = game.getClientModel();
+      var turnTracker = client.getTurn();
+      var currentPlayer = client.getPlayerWithId(currentPlayerID);
+
+      if (turnTracer.isSetupPhase){
+        if (currentPlayer.getRoads() == 0  || (currentPlayer.getRoads == 1 && currentPlayer.getSettlements() == 1){
+          this.mapController.startMove("road", true, true);
+        }
+        else if ((currentPlayer.getRoads() == 1 && currentPlayer.getSettlements() == 0) ||
+                 (currentPlayer.getRoads() == 2 && currentPlayer.getSettlements() == 1)){
+          this.mapController.startMove("settlement", true, true);
+        }
+    }
+    else{
+         window.location("catan.html");
+       }
+   };
+        
+   core.forceClassInherit(SetupRoundController,Controller);
+      
+    return SetupRoundController;
+  }());
+    
+  return SetupRoundController;
 }());
 
