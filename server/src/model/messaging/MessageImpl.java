a148f4f63f10c15fbeda09ef58906ea157859596
junetang <junetanglu@yahoo.com>
2014-03-18 20:48:57 -0600

    change chat and log model interface

---
+++ server/src/model/messaging/MessageImpl.java
@@ -0,0 +1,34 @@
+package model.messaging;
+
+import model.JsonImpl;
+import model.Model;import model.JsonImpl;
+import model.Model;
+import modelInterfaces.messaging.Line;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * Created by klu on 3/18/14.
+ */
+
+
+public class MessageImpl extends JsonImpl {
+
+    private List<Line> lines;
+
+    public MessageImpl() {
+
+        lines = new ArrayList<Line>();
+        lines.add(new LineImpl());
+        lines.add(new LineImpl());
+        lines.add(new LineImpl());
+        lines.add(new LineImpl());
+
+    }
+    public void addLine(LineImpl line){
+        lines.add(line);
+    }
+}
