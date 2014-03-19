aeb69c446172ce600710e1d6d94a062a5521f64b
todoakaio <todoakaio@gmail.com>
2014-03-18 18:56:02 -0600

    I did some pull ups

--- server/src/modelInterfaces/base/Resources.java
+++ server/src/modelInterfaces/base/Resources.java
@@ -4,4 +4,24 @@ package modelInterfaces.base;
  * Created by Jon George on 3/13/14.
  */
 public interface Resources {
+
+	public abstract void setOre(int ore);
+
+	public abstract int getOre();
+
+	public abstract void setWheat(int wheat);
+
+	public abstract int getWheat();
+
+	public abstract void setSheep(int sheep);
+
+	public abstract int getSheep();
+
+	public abstract void setWood(int wood);
+
+	public abstract int getWood();
+
+	public abstract void setBrick(int brick);
+
+	public abstract int getBrick();
 }
