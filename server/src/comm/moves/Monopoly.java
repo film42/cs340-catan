c2bd8e865a580b8a652d0da55bac5fb46695d7c9
todoakaio <todoakaio@gmail.com>
2014-03-18 22:07:44 -0600

    Monopoly Test halfway done
    Please don’t spend a lot of time creating a fake game, instead use the
    fake game that I have written in the MonopolyTest.java

--- server/src/comm/moves/Monopoly.java
+++ server/src/comm/moves/Monopoly.java
@@ -14,16 +14,14 @@ import comm.moves.base.InvalidCommandException;
  */
 public class Monopoly extends Command {
 
-    private String resource;
+	private String resource;
 
-    public String getResource() {
-        return resource;
-    }
+	public String getResource() {
+		return resource;
+	}
 
-    @Override
-    public void execute(GameInfo gameInfo) throws IOException, InvalidCommandException {
-
-		// Use monopoly card
+	@Override
+	public void execute(GameInfo gameInfo) throws IOException, InvalidCommandException {
 
 		// Get the game model
 		Game game = gameInfo.getData();
@@ -32,8 +30,68 @@ public class Monopoly extends Command {
 		int cardsStolen = 0;
 		for (Player p : game.getPlayers()) {
 			if (p.getOrderNumber() != this.playerIndex) {
-				// if p.getResources()
+				switch (resource) {
+				case "wood":
+					if (p.getResources().getWood() > 0) {
+						p.getResources().setWood(p.getResources().getWood() - 1);
+						cardsStolen++;
+					}
+					break;
+				case "sheep":
+					if (p.getResources().getSheep() > 0) {
+						p.getResources().setSheep(p.getResources().getSheep() - 1);
+						cardsStolen++;
+					}
+					break;
+				case "ore":
+					if (p.getResources().getOre() > 0) {
+						p.getResources().setOre(p.getResources().getOre() - 1);
+						cardsStolen++;
+					}
+					break;
+				case "brick":
+					if (p.getResources().getBrick() > 0) {
+						p.getResources().setBrick(p.getResources().getBrick() - 1);
+						cardsStolen++;
+					}
+					break;
+				case "wheat":
+					if (p.getResources().getWheat() > 0) {
+						p.getResources().setWheat(p.getResources().getWheat() - 1);
+						cardsStolen++;
+					}
+					break;
+				default:
+					server.Server.log.severe("Unrecognized resource selected :" + resource);
+				}
 			}
 		}
-    }
+
+		// give the qty of stolen cards to the user
+		int oldQty = 0;
+		switch (resource) {
+		case "wood":
+			oldQty = game.getPlayers().get(playerIndex).getResources().getWood();
+			game.getPlayers().get(playerIndex).getResources().setWood(oldQty + cardsStolen);
+			break;
+		case "sheep":
+			oldQty = game.getPlayers().get(playerIndex).getResources().getSheep();
+			game.getPlayers().get(playerIndex).getResources().setSheep(oldQty + cardsStolen);
+			break;
+		case "ore":
+			oldQty = game.getPlayers().get(playerIndex).getResources().getOre();
+			game.getPlayers().get(playerIndex).getResources().setOre(oldQty + cardsStolen);
+			break;
+		case "brick":
+			oldQty = game.getPlayers().get(playerIndex).getResources().getBrick();
+			game.getPlayers().get(playerIndex).getResources().setBrick(oldQty + cardsStolen);
+			break;
+		case "wheat":
+			oldQty = game.getPlayers().get(playerIndex).getResources().getWheat();
+			game.getPlayers().get(playerIndex).getResources().setWheat(oldQty + cardsStolen);
+			break;
+		default:
+			server.Server.log.severe("Unrecognized resource selected :" + resource);
+		}
+	}
 }
