2d204535e47a262888a496b04b182df7e1c70a70
junetang <junetanglu@yahoo.com>
2014-03-19 14:46:33 -0600

    YearOfPlenty command

--- server/src/comm/moves/YearOfPlenty.java
+++ server/src/comm/moves/YearOfPlenty.java
@@ -1,10 +1,16 @@
 package comm.moves;
 
+import java.io.IOException;
+
 import comm.moves.base.Command;
 import comm.moves.base.InvalidCommandException;
 import modelInterfaces.base.GameInfo;
 
-import java.io.IOException;
+import modelInterfaces.base.Player;
+
+import comm.moves.base.Command;
+import comm.moves.base.InvalidCommandException;
+import modelInterfaces.base.Resources;
 
 /**
  * Created by: film42 on: 3/13/14.
@@ -24,6 +30,34 @@ public class YearOfPlenty extends Command {
 
     @Override
     public void execute(GameInfo gameInfo) throws IOException, InvalidCommandException {
+         Player player = gameInfo.getData().getPlayers().get(getPlayerIndex());
+         addSource(player, resource1);
+         addSource(player, resource2);
 
     }
+    private void addSource(Player p, String resource){
+        switch (resource) {
+            case Resources.WOOD:
+                p.getResources().setWood(p.getResources().getWood() + 1);
+                break;
+
+            case Resources.SHEEP:
+                p.getResources().setSheep(p.getResources().getSheep() + 1);
+                break;
+
+            case Resources.ORE:
+                p.getResources().setOre(p.getResources().getOre() + 1);
+                break;
+
+            case Resources.BRICK:
+                p.getResources().setBrick(p.getResources().getBrick() + 1);
+                break;
+
+            case Resources.WHEAT:
+                p.getResources().setWheat(p.getResources().getWheat() + 1);
+                break;
+            default:
+                server.Server.log.severe("Unrecognized resource :" + resource);
+        }
+   }
 }
