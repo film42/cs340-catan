a5bf3d95b3c0a4bb271a0462fe79e167e3758483
junetang <junetanglu@yahoo.com>
2014-03-22 00:21:49 -0600

    implement command DiscardCards

--- server/src/comm/moves/DiscardCards.java
+++ server/src/comm/moves/DiscardCards.java
@@ -3,8 +3,11 @@ package comm.moves;
 import comm.moves.base.Command;
 import comm.moves.base.InvalidCommandException;
 import comm.moves.form.CardDeck;
+import modelInterfaces.base.Game;
 import modelInterfaces.base.GameInfo;
-
+import modelInterfaces.base.Player;
+import modelInterfaces.base.Resources;
+import modelInterfaces.base.TurnTracker;
 import java.io.IOException;
 
 /**
@@ -20,7 +23,45 @@ public class DiscardCards extends Command {
 
     @Override
     public void execute(GameInfo gameInfo) throws IOException, InvalidCommandException {
+        Game game = gameInfo.getData();
+        Player player = game.getPlayerByIndex(playerIndex);
+        Resources resources = player.getResources();
+        
+        if (!player.isDiscarded())
+            return;
 
+
+        if(resources.getBrick() >= discardedCards.getBrick()){
+            resources.setBrick(resources.getBrick() - discardedCards.getBrick());
+        }
+
+        if(resources.getOre()>= discardedCards.getOre()){
+            resources.setOre(resources.getOre() - discardedCards.getOre());
+        }
+
+        if(resources.getWood() >= discardedCards.getWood()){
+            resources.setWood(resources.getWood() - discardedCards.getWood());
+        }
+
+        if(resources.getWheat() >= discardedCards.getWheat()){
+            resources.setWheat(resources.getWheat() - discardedCards.getWheat());
+        }
+
+        if(resources.getSheep() >= discardedCards.getSheep()){
+            resources.setSheep(resources.getSheep() - discardedCards.getSheep());
+        }
+
+        
+        // if it is the last on discard the cards, set to robbing state
+
+        TurnTracker tracker = game.getTurnTracker();
+        if(game.isLastPlayerIndex(playerIndex)) {
+            tracker.setStatus(TurnTracker.ROBBING);
+            tracker.setCurrentTurn(0);
+        } else {
+            tracker.setCurrentTurn(tracker.getCurrentTurn() + 1);
+        }
+        
     }
 
 }
