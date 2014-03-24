4cb8b9a844ba63c01c960f664948eba60b1dfcae
Garrett Thornburg <film42@gmail.com>
2014-03-24 15:31:07 -0600

    player color now changes on join game

--- server/src/model/base/GameImpl.java
+++ server/src/model/base/GameImpl.java
@@ -229,8 +229,10 @@ public class GameImpl extends JsonImpl implements Game {
     @Override
     public void addPlayer(User user, String color) {
         for (Player player : players) {
-            if(player.getPlayerID() == user.getId())
+            if(player.getPlayerID() == user.getId()) {
+                player.setColor(color);
                 return;
+            }
         }
 
         Player newPlayer = InjectorFactory.getInjector().getInstance(Player.class);
