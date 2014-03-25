81813e5910a81cc48b9f51660e8202c6cac1829e
junetang <junetanglu@yahoo.com>
2014-03-24 18:39:59 -0600

    fixed Monopoly command

--- server/src/comm/moves/Monopoly.java
+++ server/src/comm/moves/Monopoly.java
@@ -2,10 +2,7 @@ package comm.moves;
 
 import comm.moves.base.Command;
 import comm.moves.base.InvalidCommandException;
-import modelInterfaces.base.Game;
-import modelInterfaces.base.GameInfo;
-import modelInterfaces.base.Player;
-import modelInterfaces.base.Resources;
+import modelInterfaces.base.*;
 
 import java.io.IOException;
 
@@ -28,70 +25,22 @@ public class Monopoly extends Command {
 		// Cycle through all players, steal a card, increase count, and skip if it's the current player
 		int cardsStolen = 0;
 		for (Player p : game.getPlayers()) {
-			if (p.getOrderNumber() != this.playerIndex) {
-				switch (resource.toLowerCase()) {
-				case Resources.WOOD:
-					if (p.getResources().getWood() > 0) {
-						p.getResources().setWood(p.getResources().getWood() - 1);
-						cardsStolen++;
-					}
-					break;
-				case Resources.SHEEP:
-					if (p.getResources().getSheep() > 0) {
-						p.getResources().setSheep(p.getResources().getSheep() - 1);
-						cardsStolen++;
-					}
-					break;
-				case Resources.ORE:
-					if (p.getResources().getOre() > 0) {
-						p.getResources().setOre(p.getResources().getOre() - 1);
-						cardsStolen++;
-					}
-					break;
-				case Resources.BRICK:
-					if (p.getResources().getBrick() > 0) {
-						p.getResources().setBrick(p.getResources().getBrick() - 1);
-						cardsStolen++;
-					}
-					break;
-				case Resources.WHEAT:
-					if (p.getResources().getWheat() > 0) {
-						p.getResources().setWheat(p.getResources().getWheat() - 1);
-						cardsStolen++;
-					}
-					break;
-				default:
-					server.Server.log.severe("Unrecognized resource selected: " + resource);
-				}
-			}
-
-		}
-		// give the qty of stolen cards to the user
-		int oldQty = 0;
-		switch (resource.toLowerCase()) {
-            case Resources.WOOD:
-                oldQty = game.getPlayerByIndex(playerIndex).getResources().getWood();
-                game.getPlayerByIndex(playerIndex).getResources().setWood(oldQty + cardsStolen);
-                break;
-            case Resources.SHEEP:
-                oldQty = game.getPlayerByIndex(playerIndex).getResources().getSheep();
-                game.getPlayerByIndex(playerIndex).getResources().setSheep(oldQty + cardsStolen);
-                break;
-            case Resources.ORE:
-                oldQty = game.getPlayerByIndex(playerIndex).getResources().getOre();
-                game.getPlayerByIndex(playerIndex).getResources().setOre(oldQty + cardsStolen);
-                break;
-            case Resources.BRICK:
-                oldQty = game.getPlayerByIndex(playerIndex).getResources().getBrick();
-                game.getPlayerByIndex(playerIndex).getResources().setBrick(oldQty + cardsStolen);
-                break;
-            case Resources.WHEAT:
-                oldQty = game.getPlayerByIndex(playerIndex).getResources().getWheat();
-                game.getPlayerByIndex(playerIndex).getResources().setWheat(oldQty + cardsStolen);
-                break;
-            default:
-			server.Server.log.severe("Unrecognized resource selected: " + resource);
-		}
+            if (p.getOrderNumber() != this.playerIndex) {
+                int resourceNum = p.getResources().getResourceByString(resource.toLowerCase());
+                if (resourceNum > 0) {
+                    p.getResources().setResourceByString(resource.toLowerCase(), resourceNum - 1);
+                    cardsStolen++;
+                }
+            }
+        }
+		if (cardsStolen > 0) {
+            // give the qty of stolen cards to the user
+            int oldQty = 0;
+            oldQty = game.getPlayerByIndex(playerIndex).getResources().getResourceByString(resource.toLowerCase());
+            game.getPlayerByIndex(playerIndex).getResources().setResourceByString(resource.toLowerCase(), oldQty + cardsStolen);
+            Deck deck = game.getPlayerByIndex(playerIndex).getOldDevCards();
+            deck.setMonopoly(deck.getMonopoly()-1);
+        }
 
         // Write our changes
         gameInfo.setData(game);
