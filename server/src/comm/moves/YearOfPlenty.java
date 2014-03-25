aa529678819e435482bc02137e12b9295ccde339
junetang <junetanglu@yahoo.com>
2014-03-24 18:08:57 -0600

    fixed error found in YearOfPlenty

--- server/src/comm/moves/YearOfPlenty.java
+++ server/src/comm/moves/YearOfPlenty.java
@@ -12,7 +12,7 @@ import modelInterfaces.base.Player;
 import comm.moves.base.Command;
 import comm.moves.base.InvalidCommandException;
 import modelInterfaces.base.Resources;
-
+import modelInterfaces.base.Deck;
 /**
  * Created by: film42 on: 3/13/14.
  */
@@ -36,19 +36,14 @@ public class YearOfPlenty extends Command {
          Player player = game.getPlayerByIndex(playerIndex);
          Resources resources = player.getResources();
 
-         addSource(resources, resource1);
-         addSource(resources, resource2);
+         addSource(resources, getResource1());
+         addSource(resources, getResource2());
+         Deck deck = player.getOldDevCards();
+         deck.setYearOfPlenty(deck.getYearOfPlenty()-1);
          gameInfo.setData(game);
 
     }
     private void addSource(Resources resources, String resource){
-
-        //validate resource
-        if (resources.getResourceByString(resource) < 0) {
-            server.Server.log.severe("Unrecognized resource :" + resource);
-        }
-        else {
-            resources.setResourceByString(resource, resources.getResourceByString(resource) +1);
-        }
+        resources.setResourceByString(resource, resources.getResourceByString(resource) +1);
    }
 }
