838147912ea3efbf2e67850f75ba676844214bbe
Garrett Thornburg <film42@gmail.com>
2014-03-29 00:15:02 -0600

    discard exception tests

--- server/test/comm/moves/tests/DiscardCardsTest.java
+++ server/test/comm/moves/tests/DiscardCardsTest.java
@@ -2,10 +2,13 @@ package comm.moves.tests;
 
 import comm.moves.DiscardCards;
 import comm.moves.base.Commandable;
+import comm.moves.base.InvalidCommandException;
 import model.base.GameInfoImpl;
 import modelInterfaces.base.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static comm.moves.base.Command.moveFromJson;
 import static comm.moves.tests.FakeGameFactory.FOURTH_PLAYER;
@@ -93,6 +96,51 @@ public class DiscardCardsTest {
         assertEquals(HIGH_NUMBER - 1, resources.getSheep());
         assertEquals(HIGH_NUMBER - 1, resources.getWheat());
         assertEquals(HIGH_NUMBER - 1, resources.getWood());
+    }
+
+    @Test(expected=InvalidCommandException.class)
+    public void testAlreadyDiscarded() throws IOException, InvalidCommandException {
+        // Setup
+        GameInfo fakeInfo = new GameInfoImpl(fakeWealthyGame);
+        Game game = fakeInfo.getData();
+        game.getTurnTracker().setStatus(TurnTracker.DISCARDING);
+        game.getTurnTracker().setCurrentTurn(FOURTH_PLAYER);
+
+        String json = "{ type: \"discardCards\", playerIndex: " + FOURTH_PLAYER + ", discardedCards: { " +
+                "brick: 1, ore: 1, sheep: 1, wheat: 1, wood: 1 } }";
+
+        // Create object
+        Commandable comm = moveFromJson(json, DiscardCards.class);
+
+        // Execute command
+        comm.execute(fakeInfo);
+    }
+
+    @Test(expected=InvalidCommandException.class)
+    public void testDiscardTwice() throws IOException, InvalidCommandException {
+        // Setup
+        GameInfo fakeInfo = new GameInfoImpl(fakeWealthyGame);
+        Game game = fakeInfo.getData();
+        game.getTurnTracker().setStatus(TurnTracker.DISCARDING);
+        game.getTurnTracker().setCurrentTurn(FOURTH_PLAYER);
+        Player user = game.getPlayerByIndex(FOURTH_PLAYER);
+        user.setDiscarded(false);
+
+        String json = "{ type: \"discardCards\", playerIndex: " + FOURTH_PLAYER + ", discardedCards: { " +
+                "brick: 1, ore: 1, sheep: 1, wheat: 1, wood: 1 } }";
+
+        // Create object
+        Commandable comm = moveFromJson(json, DiscardCards.class);
+
+        try {
+            comm.execute(fakeInfo);
+        } catch (Exception e) {
+            fail("Exception in should not error on first discard but did. : " + e.getMessage());
+            return;
+        }
+
+        // Exception Here
+        comm.execute(fakeInfo);
     }
 
 }
