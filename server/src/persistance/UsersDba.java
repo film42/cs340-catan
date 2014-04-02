eb3602cd949a531672e8c06eece1802ea239205d
todoakaio <todoakaio@gmail.com>
2014-04-02 13:22:43 -0600

    PersistenceProvider.java and UsersDba.java interfaces created

---
+++ server/src/persistance/UsersDba.java
@@ -0,0 +1,17 @@
+package persistance;
+
+
+public interface UsersDba {
+
+	/**
+	 * Gets the current users in the db
+	 * 
+	 * @return a List of User objects currently saved in the db
+	 */
+	public List<User> getUsers();
+
+	/**
+	 * Adds a user to the db
+	 */
+	public void addUser();
+}
